d8f492034fcb4ae72e6d3c9bf5205feb5b233141
RafaŁ Łukaszewski <solarplexus6@gmail.com>
2013-05-27 19:44:22 +0200

    Prac6Filtry: OpenCV enabled empty project

---
+++ POPrac6Filtry/POPrac6Filtry/stdafx.h
@@ -0,0 +1,15 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+
+#include "opencv2/core/core.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
